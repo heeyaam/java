9e5442fe15976438a4070f1341e0de962ea3475c
kimminjoo <kmjoo0259@naver.com>
2023-05-09 21:09:20 +0900

    Merge branch 'main' of https://github.com/heeyaam/java.git into main

--- yedam/src/chap1/homework_0509.java
+++ yedam/src/chap1/homework_0509.java
@@ -71,7 +71,7 @@ public class homework_0509 {
 		// 문제5) 차례대로 m과 n을 입력받아 m단을 n번째까지 출력하도록 하세요.
 		// 예를 들어 2와 3을 입력받았을 경우 아래처럼 출력합니다.
 		// 2 X 1 = 2
-		// 2 X 2 = 4
+		// 2 X 2 = 4 fg
 		// 2 X 3 = 6
 		System.out.println("m을 입력하시오");
 		int m = Integer.parseInt(scanner.nextLine());
