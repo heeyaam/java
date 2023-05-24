d56a18f39b25073021d8ffb8c1f303395c2e198a
kimminjoo <kmjoo0259@naver.com>
2023-05-24 20:47:57 +0900

    깃테스트 변경사항 저장

--- chap01/src/com/yedam/chap01/example01.java
+++ chap01/src/com/yedam/chap01/example01.java
@@ -3,9 +3,9 @@ package com.yedam.chap01;
 public class example01 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello Wooooo");
 		// 한줄 주석
 		/* 여러즐 주석*/
-		
+		//실험하는중이다다다다아ㅏㅇ
 	}
 }
