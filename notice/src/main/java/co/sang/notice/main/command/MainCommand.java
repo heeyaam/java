d1b209acd583d7233345f38bd39127faf52ca84c
heeyaam <132875204+heeyaam@users.noreply.github.com>
2023-06-02 17:48:31 +0900

    Add files via upload

--- notice/src/main/java/co/sang/notice/main/command/MainCommand.java
+++ notice/src/main/java/co/sang/notice/main/command/MainCommand.java
@@ -10,8 +10,8 @@ public class MainCommand implements Command {
 	@Override
 	public String exec(HttpServletRequest request, HttpServletResponse restponse) {
 		// main.jsp 돌려주는 곳
-		return "main/main";    //
-		
+		return "main/main"; //
+
 	}
 
 }
